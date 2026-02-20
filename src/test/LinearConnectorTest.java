595f91acd5b473c2b5d3aade88c5278dbd6f6c7f
Aneesh Arunjunai <aneesharunjunai@gmail.com>
2026-02-19 20:20:55 -0500

    test(SIG-9): add WireMock integration tests for Linear connector

---
+++ src/test/LinearConnectorTest.java
@@ -0,0 +1,5 @@
+// SIG-9: WireMock stubs for issueCreate, issueUpdate, commentCreate
+public class LinearConnectorTest {
+    @Test void testIssueCreate() {}
+    @Test void testCommentCreate() {}
+}
